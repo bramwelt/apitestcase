f6f08a2b69fbb0784e256241be69e3d3c7b5d56d
Trevor Bramwell <trevor@bramwell.net>
2014-07-20 13:38:11 -0700

    Move endpoints in integration tests into functions

--- test/integration.py
+++ test/integration.py
@@ -8,24 +8,23 @@ class TestGetMethod(unittest.TestCase, apitestcase.TestCase):
     Integration test on GET methods
     """
     hosts = ["http://httpbin.org"]
-    endpoint = "/"
 
     def test_get(self):
-        self.assertGet(self.endpoint)
+        self.assertGet("/")
 
     def test_get_ip(self):
-        self.assertGet(self.endpoint+"ip", body="origin")
+        self.assertGet("/ip", body="origin")
 
     def test_get_ua(self):
-        self.assertGet(self.endpoint+"user-agent",
+        self.assertGet("/user-agent",
                        body="python-requests")
 
     def test_get_headers(self):
-        self.assertGet(self.endpoint+"headers",
+        self.assertGet("/headers",
                        body=["\"Host\": \"httpbin.org\"",
                              "X-Request-Id",
                              "Accept"])
 
     def test_get_args(self):
-        self.assertGet(self.endpoint+"get?foo=bar",
+        self.assertGet("/get?foo=bar",
                        body="\"foo\": \"bar\"")
